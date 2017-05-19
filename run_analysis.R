e1d65a9fa9381baa8daa3f73665d23a59d53cb37
keeseveleensmaarse <cs.eveleens.maarse@portofrotterdam.com>
2017-05-19 09:37:48 +0200

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,34 @@
-# This script produces a single summary table that can be used for later analysis. It
-# requires the availability of a number of datafiles in the working directory (see below).
-# These datafiles contain data collected from the accelerometers from the Samsung Galaxy S smartphone.
-# The summary table named "total_set_mean" gives some summary data per testperson, per activity type.
-# 
-# First, read in all data tables. The relevant tables are named in accordance with   
-# the name of the sources. 
-# It is assumed that all files are present in the current working directory.
 #
-features          <- read.table("features.txt", header = FALSE, sep = "")
-activity_labels   <- read.table("activity_labels.txt", header = FALSE, sep = "")
-y_test            <- read.table("y_test.txt", header = FALSE, sep = "")
-y_train           <- read.table("y_train.txt", header = FALSE, sep = "")
-subject_test      <- read.table("subject_test.txt", header = FALSE, sep = "")
-subject_train     <- read.table("subject_train.txt", header = FALSE, sep = "")
-X_test            <- read.table("X_test.txt", header = FALSE, sep = "", fill = TRUE)
-X_train           <- read.table("X_train.txt", header = FALSE, sep = "", fill = TRUE)
+# Start with downloading and unzipping the files from the source as indicated 
+#
+URL <- ”https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip”
+files_to_unzip    <- c("UCI HAR Dataset/activity_labels.txt","UCI HAR Dataset/features.txt","UCI HAR Dataset/test/subject_test.txt","UCI HAR Dataset/train/subject_train.txt","UCI HAR Dataset/test/X_test.txt","UCI HAR Dataset/train/X_train.txt","UCI HAR Dataset/test/y_test.txt","UCI HAR Dataset/train/y_train.txt")
+download.file(URL,"data.zip", mode="wb")
+unzip("data.zip",files = files_to_unzip,exdir="./data",junkpaths=TRUE)
+#
+# Read in all data tables. The relevant tables are named in accordance with   
+# the name of the sources. 
+#
+features          <- read.table("./data/features.txt", header = FALSE, sep = "")
+activity_labels   <- read.table("./data/activity_labels.txt", header = FALSE, sep = "")
+y_test            <- read.table("./data/y_test.txt", header = FALSE, sep = "")
+y_train           <- read.table("./data/y_train.txt", header = FALSE, sep = "")
+subject_test      <- read.table("./data/subject_test.txt", header = FALSE, sep = "")
+subject_train     <- read.table("./data/subject_train.txt", header = FALSE, sep = "")
+X_test            <- read.table("./data/X_test.txt", header = FALSE, sep = "", fill = TRUE)
+X_train           <- read.table("./data/X_train.txt", header = FALSE, sep = "", fill = TRUE)
+#
+# First of all, we need to select the columns containing the required data.
+# I do this by finding the columns containing ‘std’ and ‘mean’ in the names.
+#
+col_std           <- grep("std", features[,2])  
+col_mean          <- grep("mean", features[,2])
 #
 # Then, we start working with the ‘test’-tables. A number of columns is selected out 
 # of the X_test table, and stored as X_test_col. Then an id is added to the table,    
 # for merging purpose. This id is also added to subject_test and to y_test. 
 #
-X_test_col        <- X_test[,c(1,2,3,4,5,6,121,122,123,124,125,126)]
+X_test_col        <- X_test[,c(col_std, col_mean)]
 X_test_col$id     <- 1:nrow(X_test_col)
 subject_test$id   <- 1:nrow(subject_test)
 y_test$id         <- 1:nrow(y_test)
@@ -32,10 +40,11 @@ y_test$id         <- 1:nrow(y_test)
 X_test_col_sub    <- merge(X_test_col,subject_test, by = "id")
 X_test_col_sub_y  <- merge(X_test_col_sub,y_test, by = "id")
 #
+
 # Now we do a similar actions for the ‘train’-tables. So, first select the columns   
 # and add the id; the latter also for subject_train and y_train.
 # 
-X_train_col       <- X_train[,c(1,2,3,4,5,6,121,122,123,124,125,126)]
+X_train_col       <- X_train[,c(col_std,col_mean)]
 X_train_col$id    <- 1:nrow(X_train_col)
 subject_train$id  <- 1:nrow(subject_train)
 y_train$id        <- 1:nrow(y_train)
@@ -50,39 +59,38 @@ X_train_col_sub_y <- merge(X_train_col_sub,y_train, by = "id")
 #
 X_total_col_sub_y <- rbind(X_test_col_sub_y, X_train_col_sub_y)
 #
-# Now we remove the ‘ Id’-column - it is not needed any more. And we change the order 
-# of the columns in the table, so that Subject and Activity come first.
-#
-X_total_col_sub_y <- X_total_col_sub_y[ -c(1)]
-X_total_col_sub_y <- X_total_col_sub_y[ c(13, 14, 1:12)]
-#
 # We still need to decode the Activity, so that it will not have values like 1, 2 and 
 # so on, but “ LAYING, STANDING etc. This again is a Merge action, this time with    
 # table named activity_labels, on column V1.
 #
 X_total_col_sub_y <- merge(X_total_col_sub_y,activity_labels, by = "V1")
 #
-# Again change the order of the columns, because the merge-action put them in an     
-# order we don’t want.
+# We do not require the first two columns V1 and Id any more
 #
-X_total_col_sub_y <- X_total_col_sub_y[ c(2, 15, 3:14)]
+X_total_col_sub_y <- X_total_col_sub_y[,c(-1,-2)]
 #
 # Nearly there! Now, we need to give appropriate names to the various columns.
 #
-colnames(X_total_col_sub_y) <- c("Subject", "Activity", "BodyAcc_mean_X","BodyAcc_mean_Y", "BodyAcc_mean_Z", "BodyAcc_std_X","BodyAcc_std_Y", "BodyAcc_std_Z", "BodyGyro_mean_X", "BodyGyro_mean_Y", "BodyGyro_mean_Z", "BodyGyro_std_X","BodyGyro_std_Y", "BodyGyro_std_Z")
+colnames(X_total_col_sub_y) <- c(as.character(features[c(col1,col2),2]), "Subject", "Activity")
 #
 # Done! Just give a nice name to the result, and we’re done!
 #
-total_set <- X_total_col_sub_y
+total_set         <- X_total_col_sub_y
 #
 #
 # Then, there’s the question for a second data set containing the averages for each  
-# activity and each subject. I used ‘Group by’ and ‘summarise’ to realise this. And  
-# changed the names of the columns, so that they reflect a difference with the       
-# previous data set. The name of this data set is total_set_mean.  
+# activity and each subject. I used ‘Group by’ and ‘summarise_each’ to realise this.      
+# The name of this data set is total_set_mean. 
 #
-total_set_group <- group_by(total_set, Subject, Activity)
-total_set_mean <- summarise(total_set_group, MeanBodyAcc_mean_X = mean(BodyAcc_mean_X), MeanBodyAcc_mean_Y = mean(BodyAcc_mean_Y), MeanBodyAcc_mean_Z = mean(BodyAcc_mean_Z), MeanBodyAcc_std_X = mean(BodyAcc_std_X), MeanBodyAcc_std_Y = mean(BodyAcc_std_Y), MeanBodyAcc_std_Z = mean(BodyAcc_std_Z), MeanBodyGyro_mean_X = mean(BodyGyro_mean_X), MeanBodyGyro_mean_Y = mean(BodyGyro_mean_Y), MeanBodyGyro_mean_Z = mean(BodyGyro_mean_Z), MeanBodyGyro_std_X = mean(BodyGyro_std_X), MeanBodyGyro_std_Y = mean(BodyGyro_std_Y), MeanBodyGyro_std_Z = mean(BodyGyro_std_Z))
+# First, the dplyr-package needs to be included.
+#
+library(dplyr)
+total_set_group   <- group_by(total_set, Subject, Activity)
+total_set_mean    <- summarise_each(total_set_group, "mean")
+#
+# And write the result into a file.
+#
+write.table(total_set_mean, file = "total_set_mean.txt", row.name = FALSE)
 #
 # End
 #
